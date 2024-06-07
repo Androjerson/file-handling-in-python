5e4537bcfbf73b2d9f51e46ba67130c89115d1ab
Androjerson <jersonfenadictandro@gmail.com>
2024-06-07 10:36:14 +0530

    Updating changes

--- file handling-python.py
+++ file handling-python.py
@@ -14,7 +14,14 @@ with open("file handling copy.txt","r") as copied_file:
 
 # Write method completely rewrites the file, thus if you wanna append something to a file , use append mode 
 with open("file handling copy.txt","a") as appending_file:
-    appending_file.write("I am enthusuastic")
+    appending_file.write("\nI am enthusuastic")
+
+# Opening and copying a image file
+with open("Virat-Kohli.png","rb") as src_img:
+    image=src_img.read() #Displays the binary data 
+
+with open("Virat-Kohli-cp.png","wb") as tgt_img:
+        tgt_img.write(image)
 
 
 
